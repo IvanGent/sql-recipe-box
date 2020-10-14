7bb6ecac7ff0d8968ffdcbc73391c2acc4ae1f23
Rhys Previte <previterp88@gmail.com>
2020-10-14 14:39:34 -0500

    Completed up until 02b

--- data-access-layer/02b-get-ingredients-by-recipe-id.sql
+++ data-access-layer/02b-get-ingredients-by-recipe-id.sql
@@ -38,4 +38,8 @@
 -- JOIN weather ON (weather.city = cities.name)
 
 
--- YOUR CODE HERE
+SELECT amount, food_stuff, name
+FROM ingredients
+JOIN units_of_measure ON (unit_of_measure_id = units_of_measure.id)
+WHERE ingredients.recipe_id = $1; 
+
